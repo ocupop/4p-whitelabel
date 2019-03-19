048b8a6c0fa5de7a9a90e5ac01e9713cc19968af
Tom Beck <tom@ocupop.com>
2019-03-19 15:33:53 -0700

    cookie test

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -1,4 +1,6 @@
-let showIntro = Cookies.get('inova-intro')
+// Cookie Management
+
+let showIntro = Cookies.get('inova-intro');
 if (!showIntro) {
   // $('#introModal').modal('show');
   console.log("Add show modal");
@@ -7,3 +9,19 @@ if (!showIntro) {
 $('#introModal').on('shown.bs.modal', function () {
   Cookies.set('inova-intro', true)
 });
+
+// const ocuCookies = {
+//   path: '/',
+//   domain: '',
+//   expires: '',
+//   secure: true
+// }
+// Cookies.defaults = {
+//   path: '/',
+//   secure: true
+// };
+
+// Cookies.set('key', 'value'); // Will be secure and have a path of '/'
+// Cookies.expire('key'); // Will expire the cookie with a path of '/'
+
+console.log(Cookies.get('wordpress_test_cookie'));
