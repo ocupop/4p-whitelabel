9cfc6c1e4443813539084d6ec19a5d60078416f2
Tom Beck <tom@ocupop.com>
2019-03-19 17:15:58 -0700

    updated the cookie logic to test

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -1,27 +1,17 @@
 // Cookie Management
+Cookies.defaults = {
+  path: '/',
+  domain: '4pfoods.com',
+  secure: true
+};
 
 let showIntro = Cookies.get('inova-intro');
 if (!showIntro) {
   // $('#introModal').modal('show');
-  console.log("Add show modal");
+  Cookies.set('inova-intro', true);
 }
 
 $('#introModal').on('shown.bs.modal', function () {
-  Cookies.set('inova-intro', true)
+  Cookies.set('inova-intro', true);
 });
 
-// const ocuCookies = {
-//   path: '/',
-//   domain: '',
-//   expires: '',
-//   secure: true
-// }
-// Cookies.defaults = {
-//   path: '/',
-//   secure: true
-// };
-
-// Cookies.set('key', 'value'); // Will be secure and have a path of '/'
-// Cookies.expire('key'); // Will expire the cookie with a path of '/'
-
-console.log(Cookies.get('wordpress_test_cookie'));
